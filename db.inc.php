8262746a929baeb01ebfce1877fe347644d71996
dbel6 <dbel.631@gmail.com>
2026-04-22 15:40:28 +0100

    Add files via upload

---
+++ db.inc.php
@@ -0,0 +1,29 @@
+<!--
+    Student Name : 		Daniel Belov
+    Student Id Number : CO0306133
+    Date :				23/2/2026
+    Purpose : 			To create connection to the database
+-->
+
+<?php
+/*Creates a space allowing for calculating user input*/
+$hostname = "localhost";
+//Name of host or ip address
+$username = "CarRentalThree";
+//MySQL username
+$password = "CarRentalThr33";
+//MySQL password
+
+$dbname = "CarRental3";
+//database name
+
+$con = mysqli_connect($hostname, $username, $password, $dbname);
+//Connects to the database by sending hostname, username, password and database name
+
+if (!$con)
+//Outputs a message if database failed to connect
+{
+    die ("Failed to connect to MySQL: " . mysqli_connect_error());
+    //Outputs a message saying the MySQL failed to be connected to and sets status to error
+}
+?>
---
+++ menu.php
@@ -0,0 +1,39 @@
+<!--
+    Student Name : 		Daniel Belov
+    Student Id Number : CO0306133
+    Date :				5/3/2026
+    Purpose : 			To complete menu part of Company screen
+-->
+
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="Style.css">
+    <!--Link to css file which styles the form for better presentation-->
+</head>
+<body>
+
+    <div class="form">
+    <!--Displays a menu where user can select buttons-->
+
+    <div class="menu">
+    <!--Displays a menu area where user can select buttons-->
+
+    <img src="logo-fixed.png" height="50" width="50">
+    <!--Logo image that fits in the menu-->
+
+    <a href="menu.php">Home</a>
+    <!--Home button--> 
+    <a href="AddNewCarCompany.html.php">Add Company</a>
+    <!--Add Company button-->
+    <a href="DeleteCarCompany.html.php">Delete Company</a>
+    <!--Delete Company button-->
+    <a href="AmendViewCarCompany.html.php">Amend/View Company</a>
+    <!--Update Company button-->
+    <a href="Rentals.html.php">Rentals</a>
+    <!--Rentals button--> 
+
+</div>
+</div>
+</body>
+</html>
